113a94db6dfc5dbfd1cf6ca4a4710946210568aa
RianC4rl0s <riancarlos.sl@gmail.com>
2022-04-20 00:52:22 -0300

    product

--- app/src/main/java/com/example/myapplication/DataBase.java
+++ app/src/main/java/com/example/myapplication/DataBase.java
@@ -22,6 +22,13 @@ public class DataBase extends SQLiteOpenHelper {
     private static final String COLUMN_PHONE = "cl_phone";
     private static final String COLUMN_EMAIL = "cl_email";
 
+
+    private static final String TABLE_PRODUCT = "tb_product";
+    private static final String COLUMN_PRODUCT_ID = "cl_id";
+    private static final String COLUMN_PRODUCT_NAME = "cl_name";
+    private static final String  COLUMN_PRODUCT_PRICE= "cl_price";
+    private static final String COLUMN_PRODUCT_QTD = "cl_qtd";
+    private static final String COLUMN_PRODUCT_MARCA = "cl_marca";
     public DataBase( Context context) {
         super(context, DATABASE, null, VERSION);
     }
@@ -33,6 +40,13 @@ public class DataBase extends SQLiteOpenHelper {
                 + COLUMN_NAME +  " TEXT,"
                 + COLUMN_PHONE + " TEXT, "
                 + COLUMN_EMAIL + " TEXT )";
+        sqLiteDatabase.execSQL(QUERY_TABLE);
+        QUERY_TABLE = "CREATE TABLE " + TABLE_PRODUCT + " ( "
+                + COLUMN_PRODUCT_ID + " INTEGER PRIMARY KEY, "
+                + COLUMN_PRODUCT_NAME +  " TEXT,"
+                + COLUMN_PRODUCT_PRICE + " REAL, "
+                + COLUMN_PRODUCT_QTD + " INT,"
+                + COLUMN_PRODUCT_MARCA + " TEXT )";
         sqLiteDatabase.execSQL(QUERY_TABLE);
     }
 
@@ -53,9 +67,27 @@ public class DataBase extends SQLiteOpenHelper {
         sqLiteDatabase.close();
 
     }
+    void addProduct(Product p){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues values  = new ContentValues();
+
+        values.put(COLUMN_PRODUCT_NAME,p.getName());
+        values.put(COLUMN_PRODUCT_PRICE,p.getPrice());
+        values.put(COLUMN_PRODUCT_QTD,p.getQdt());
+        values.put(COLUMN_PRODUCT_MARCA,p.getMarca());
+
+        sqLiteDatabase.insert(TABLE_PRODUCT,null,values);
+        sqLiteDatabase.close();
+
+    }
     void deleteClient(Client client){
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_CLIENT,COLUMN_ID + " = ? ", new String[] {String.valueOf(client.getCode())} );
+        db.delete(TABLE_CLIENT,COLUMN_PRODUCT_ID + " = ? ", new String[] {String.valueOf(client.getCode())} );
+
+    }
+    void deleteProduct(Product p){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_PRODUCT,COLUMN_ID + " = ? ", new String[] {String.valueOf(p.getCode())} );
 
     }
     Client selectClient(int id){
@@ -70,6 +102,18 @@ public class DataBase extends SQLiteOpenHelper {
         cursor.close();
         return client1;
     }
+    Product selectProduct(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_PRODUCT,new String[] {COLUMN_PRODUCT_ID,COLUMN_PRODUCT_NAME,COLUMN_PRODUCT_PRICE,COLUMN_PRODUCT_QTD,COLUMN_PRODUCT_MARCA},
+                COLUMN_ID + " = ?" , new String[] {String.valueOf(id)},
+                null,null,null,null);
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+        Product p = new Product(Integer.parseInt(cursor.getString(0)),cursor.getString(1),Double.parseDouble(cursor.getString(2)),Integer.parseInt(cursor.getString(3)),cursor.getString(4));
+        cursor.close();
+        return p;
+    }
     void updateClient(Client client){
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -80,6 +124,18 @@ public class DataBase extends SQLiteOpenHelper {
 
         db.update(TABLE_CLIENT,values,COLUMN_ID + "  = ? ",
                 new String[] {String.valueOf(client.getCode())});
+    }
+    void updateProduct(Product p){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values  = new ContentValues();
+        values.put(COLUMN_PRODUCT_NAME,p.getName());
+        values.put(COLUMN_PRODUCT_PRICE,p.getPrice());
+        values.put(COLUMN_PRODUCT_QTD,p.getQdt());
+        values.put(COLUMN_PRODUCT_MARCA,p.getQdt());
+
+        db.update(TABLE_CLIENT,values,COLUMN_ID + "  = ? ",
+                new String[] {String.valueOf(p.getCode())});
     }
     public List<Client> listaAllClients(){
         List<Client> clients = new ArrayList<>();
@@ -95,5 +151,20 @@ public class DataBase extends SQLiteOpenHelper {
         }
         cursor.close();
         return clients;
+    }
+    public List<Product> listProducts(){
+        List<Product> products = new ArrayList<>();
+
+        String query = "SELECT * FROM " + TABLE_PRODUCT;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query,null);
+        if(cursor.moveToFirst()){
+            do {
+                Product p = new Product(Integer.parseInt(cursor.getString(0)),cursor.getString(1),Double.parseDouble(cursor.getString(2)),Integer.parseInt(cursor.getString(3)),cursor.getString(4));
+                products.add(p);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return products;
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Product.java
@@ -0,0 +1,65 @@
+package com.example.myapplication;
+
+public class Product {
+    int code;
+    String name;
+    Double Price;
+    int qdt;
+    String marca;
+
+    public Product(){};
+
+    public Product(int code, String name, Double price, int qdt, String marca) {
+        this.code = code;
+        this.name = name;
+        Price = price;
+        this.qdt = qdt;
+        this.marca = marca;
+    }
+    public Product( String name, Double price, int qdt, String marca) {
+        this.name = name;
+        Price = price;
+        this.qdt = qdt;
+        this.marca = marca;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(Double price) {
+        Price = price;
+    }
+
+    public int getQdt() {
+        return qdt;
+    }
+
+    public void setQdt(int qdt) {
+        this.qdt = qdt;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
